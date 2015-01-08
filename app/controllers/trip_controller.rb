991631cb54bdc43dbebbe4a0db872803bfad0e1e
alfredcalayag <rapadura05@gmail.com>
2015-01-08 11:54:40 -0800

    Fixed new_trip form route. Fixed trip controller

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -1,13 +1,14 @@
 # Trip controller
 
 # Provide a new trip form
-get '/trips/new' do
-  erb :new_trip
+get '/users/:user_id/trips/new' do
+  @user_id = params[:user_id]
+  erb :"trips/new_trip"
 end
 
 
 # Create a new trip for a specific user
-post '/trips' do
+post '/users/:user_id/trips' do
   reminder_minutes = params[:reminder_minutes].to_i
   depart_time = DateTime.parse(params[:depart_time])
   reminder_time = depart_time - reminder_minutes.minutes
@@ -18,9 +19,12 @@ post '/trips' do
     destination: params[:destination],
     reminder_minutes: params[:reminder_minutes],
     depart_time: depart_time,
-    reminder_time: reminder_time
+    reminder_time: reminder_time,
+    user_id: params[:user_id].to_i
   }
 
   new_trip = Trip.create(new_trip_data)
-  redirect '/trips'
+  redirect "/users/#{params[:user_id]}"
 end
+
+
